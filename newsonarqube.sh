d005414bc5cfc0a6cd65215f251cd84e1aca7764
saabbas88 <85310420+saabbas88@users.noreply.github.com>
2025-08-26 11:09:10 +0530

    Update newsonarqube.sh

--- newsonarqube.sh
+++ newsonarqube.sh
@@ -4,3 +4,7 @@ sudo apt install docker -y
 sudo service docker start
 sudo usermod -aG docker ubuntu
 exit
+
+Run Sonarqube using docker image :
+docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube:lts-community
+
