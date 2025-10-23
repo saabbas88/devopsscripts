39266d09101f765c0562dd746f2ef22b68a080b4
saabbas88 <85310420+saabbas88@users.noreply.github.com>
2025-10-23 18:15:00 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,14 @@
 #STEP-1: Installing Git and Maven
 yum install git maven -y
+yum install wget -y
 
-#STEP-2: Repo Information (jenkins.io --> download -- > redhat)
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+#STEP-2: Repo Information (jenkins.io --> download -- > amazon-linux/redhat)
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 #STEP-3: Download Java 17 and Jenkins
-sudo yum install java-17-amazon-corretto -y
+sudo yum install fontconfig java-21-openjdk
 yum install jenkins -y
 
 #STEP-4: Start and check the JENKINS Status
