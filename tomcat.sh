b413d813ccd574365cadbab6cee6821106ebeaec
saabbas88 <85310420+saabbas88@users.noreply.github.com>
2026-08-10 02:07:23 +0530

    Update tomcat.sh for Tomcat 11.0.24 configuration

--- tomcat.sh
+++ tomcat.sh
@@ -4,7 +4,7 @@ tar -zxvf apache-tomcat-11.0.24.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.24/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.24/conf/tomcat-users.xml
 sed -i '58  a\<user username="tomcat" password="root123456" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.24/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.14/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.24/conf/tomcat-users.xml
 sed -i '56d' apache-tomcat-11.0.24/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-11.0.24/webapps/manager/META-INF/context.xml
 sed -i '22d' apache-tomcat-11.0.24/webapps/manager/META-INF/context.xml
